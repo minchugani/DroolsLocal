fe7ab58fec7cd8c82f552e954095cb78473fbfd0
srh001e <sowjanya.hegde@walmart.com>
2020-05-08 19:37:21 +0530

    Drools

---
+++ src/main/java/com/example/demo/Michugani1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Michugani1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Michugani1Application.class, args);
+	}
+
+}
